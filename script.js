b5ac8b11244dbead88be15395a215f0ac8913544
Rich Weiss <richweissnyc@gmail.com>
2016-07-22 17:36:53 -0400

    Added deck of cards object

---
+++ script.js
@@ -0,0 +1,290 @@
+var deck = [
+  {
+  suit: "hearts";
+  value: "two";
+  facecard: "false";
+  facevalue: "two";
+  },
+  {
+  suit: "hearts";
+  value: "three";
+  facecard: "false";
+  facevalue: "three";
+  },
+  {
+  suit: "hearts";
+  value: "four";
+  facecard: "false";
+  facevalue: "four";
+  },
+  {
+  suit: "hearts";
+  value: "five";
+  facecard: "false";
+  facevalue: "five";
+  },
+  {
+  suit: "hearts";
+  value: "six";
+  facecard: "false";
+  facevalue: "six";
+  },
+  {
+  suit: "hearts";
+  value: "seven";
+  facecard: "false";
+  facevalue: "seven";
+  },
+  {
+  suit: "hearts";
+  value: "eight";
+  facecard: "false";
+  facevalue: "eight";
+  },
+  {
+  suit: "hearts";
+  value: "nine";
+  facecard: "false";
+  facevalue: "nine";
+  },
+  {
+  suit: "hearts";
+  value: "ten";
+  facecard: "false";
+  facevalue: "ten";
+  },
+  {
+  suit: "hearts";
+  value: "ten";
+  facecard: "true";
+  facevalue: "Jack";
+  },
+  {
+  suit: "hearts";
+  value: "ten";
+  facecard: "true";
+  facevalue: "Queen";
+  },
+  {
+  suit: "hearts";
+  value: "ten";
+  facecard: "true";
+  facevalue: "King";
+  },
+  {
+  suit: "diamonds";
+  value: "two";
+  facecard: "false";
+  facevalue: "two";
+  },
+  {
+  suit: "diamonds";
+  value: "three";
+  facecard: "false";
+  facevalue: "three";
+  },
+  {
+  suit: "diamonds";
+  value: "four";
+  facecard: "false";
+  facevalue: "four";
+  },
+  {
+  suit: "diamonds";
+  value: "five";
+  facecard: "false";
+  facevalue: "two";
+  },
+  {
+  suit: "diamonds";
+  value: "six";
+  facecard: "false";
+  facevalue: "six";
+  },
+  {
+  suit: "diamonds";
+  value: "seven";
+  facecard: "false";
+  facevalue: "seven";
+  },
+  {
+  suit: "diamonds";
+  value: "eight";
+  facecard: "false";
+  facevalue: "eight";
+  },
+  {
+  suit: "diamonds";
+  value: "nine";
+  facecard: "false";
+  facevalue: "nine";
+  },
+  {
+  suit: "diamonds";
+  value: "ten";
+  facecard: "false";
+  facevalue: "ten";
+  },
+  {
+  suit: "diamonds";
+  value: "ten";
+  facecard: "true";
+  facevalue: "Jack";
+  },
+  {
+  suit: "diamonds";
+  value: "ten";
+  facecard: "true";
+  facevalue: "Queen";
+  },
+  {
+  suit: "diamonds";
+  value: "ten";
+  facecard: "true";
+  facevalue: "King";
+  },
+  {
+  suit: "spades";
+  value: "two";
+  facecard: "false";
+  facevalue: "two";
+  },
+  {
+  suit: "spades";
+  value: "three";
+  facecard: "false";
+  facevalue: "three";
+  },
+  {
+  suit: "spades";
+  value: "four";
+  facecard: "false";
+  facevalue: "four";
+  },
+  {
+  suit: "spades";
+  value: "five";
+  facecard: "false";
+  facevalue: "five";
+  },
+  {
+  suit: "spades";
+  value: "six";
+  facecard: "false";
+  facevalue: "six";
+  },
+  {
+  suit: "spades";
+  value: "seven";
+  facecard: "false";
+  facevalue: "seven";
+  },
+  {
+  suit: "spades";
+  value: "eight";
+  facecard: "false";
+  facevalue: "eight";
+  },
+  {
+  suit: "spades";
+  value: "nine";
+  facecard: "false";
+  facevalue: "nine";
+  },
+  {
+  suit: "spades";
+  value: "ten";
+  facecard: "false";
+  facevalue: "ten";
+  },
+  {
+  suit: "spades";
+  value: "ten";
+  facecard: "true";
+  facevalue: "Jack";
+  },
+  {
+  suit: "spades";
+  value: "ten";
+  facecard: "true";
+  facevalue: "Queen";
+  },
+  {
+  suit: "spades";
+  value: "ten";
+  facecard: "true";
+  facevalue: "King";
+  },
+  {
+  suit: "clubs";
+  value: "two";
+  facecard: "false";
+  facevalue: "two";
+  },
+  {
+  suit: "clubs";
+  value: "three";
+  facecard: "false";
+  facevalue: "three";
+  },
+  {
+  suit: "clubs";
+  value: "four";
+  facecard: "false";
+  facevalue: "four";
+  },
+  {
+  suit: "clubs";
+  value: "five";
+  facecard: "false";
+  facevalue: "five";
+  },
+  {
+  suit: "clubs";
+  value: "six";
+  facecard: "false";
+  facevalue: "six";
+  },
+  {
+  suit: "clubs";
+  value: "seven";
+  facecard: "false";
+  facevalue: "seven";
+  },
+  {
+  suit: "clubs";
+  value: "eight";
+  facecard: "false";
+  facevalue: "eight";
+  },
+  {
+  suit: "clubs";
+  value: "nine";
+  facecard: "false";
+  facevalue: "nine";
+  },
+
+]
+
+
+
+// var begin = document.getElementsbyClass(".deck")
+
+// function begin(){
+//   addPlayerListener();
+//   addDealerListener();
+// }
+
+// function addPlayerCard1Listener(){
+//   for (var i = 0; i >= 0; i--) {
+//     boxes[i].addEventListener("click", ??);
+//   }
+
+// function addPlayerCard2Listener(){
+// }
+
+// function addDealerCard1Listener(){
+//   }
+
+// function addDealerCard2Listener(){
+//   }
