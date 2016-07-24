a1896e218faa248c8895505e64a886430648274e
Rich Weiss <richweissnyc@gmail.com>
2016-07-24 18:19:07 -0400

    facedown image on cards

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+//PSUEDOCODE taken from notes
+// BLACKJACK
+
+// *Break down : Data, Presentation, Views, Style, DOM manipulation*
+
+// create game board
+// create deck
+
+// Game should contain:
+
+// 1. Deck of Cards (clickable)
+// 2. Dealer Hand (two cards)
+// 3. Player Hand (two cards)
+// 4. Extra Slots for HIT Cards
+// 5. Hit/Stay Button for Player
+// 6. Shuffle Button
+// 7. Reset Button
+// 8. Wins/Losses Counter
+// 9. *Extra* betting component
+
+// PseudoCode
+// Create array for randomized Deck of Cards.
+
+// Create objects for Dealer Hand,
+//   - -D <= 16 - HIT ALWAYS
+//          -D < 16 - STAY ALWAYS
+
+// Player Hand
+// Create If/Else - Player HIT or STAY.
+//  - Hit will POP or SHIFT from deck.
+
+// After player selects STAY, dealer compares total values of Dealer Hand vs Player hand
+
+// getWINNER
+//  total value of Player > total Value of Dealer
+//   EXCEPT if total value is > 21
+
+// Wins/Losses counter tallies results and displays on left-hand side of page.
+
+// Create SHUFFLE button  that will randomize deck.
+
+// Problems:
+//  Do I default Ace to 11 or 1 or can I have those values switch?
+
+// Can I assign images on each of my individual cards?
+
+// Can I get deck to shuffle after a few hands?
+
 //create deck//
 var deckArray = [
   {
@@ -297,6 +345,9 @@ var dealerCard1 = getElementbyID("dealerCard1")
 var dealerCard2 = getElementbyID("dealerCard2")
 var playerCard1 = getElementbyID("playerCard1")
 var playerCard2 = getElementbyID("playerCard2")
+//create overall value of each hand for checkforWinner//
+var dealerValue = (dealerCard1.value) + (dealercard2.value)
+var playerValue = (playercard1.value) + (playercard2.value)
 //create function to random deal THEN assign values to each ID//
 var randomCard = function() {
   var deal = Math.floor(Math.random(deckArray.length);
@@ -325,6 +376,6 @@ function addPlayListener(){
 }
 
 
-function CheckforWin () {
-  if
+var checkforWin = function () {
+  if ()
 }
